277e9840edfec157ddcefa9306c3882a92a743d5
Bartosz Iwiński <01bartosz0001@gmail.com>
2019-08-06 11:13:06 +0200

    Add scripts in scipt.js, add hover to class answer in style.scss, modified id's in index.html

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+	var ans1 = document.getElementById('ans1');
+	var ans2 = document.getElementById('ans2');
+	var ans3 = document.getElementById('ans3');
+	
+	ans1.addEventListener("click", function() { check(1); });
+	ans2.addEventListener("click", function() { check(2); });
+	ans3.addEventListener("click", function() { check(3); });
+	
+	function check(playerMove) {
+		let computerMove = Math.floor(Math.random() * 3 + 1);
+		let result;
+		if(computerMove===1 && playerMove===1 || computerMove===2 && playerMove===2 || computerMove===3 && playerMove===3) result = 'There is a draw.';
+		else if(computerMove===1 && playerMove===2 || computerMove===2 && playerMove===3 || computerMove===3 && playerMove===1) result = 'You win!';
+		else if(computerMove===1 && playerMove===3 || computerMove===2 && playerMove===1 || computerMove===3 && playerMove===2) result = 'You lose!';
+		if(computerMove===1) computerMove = 'paper';
+		else if(computerMove===2) computerMove = 'scatters';
+		else if(computerMove===3) computerMove = 'stone';
+		if(playerMove===1) playerMove = 'paper';
+		else if(playerMove===2) playerMove = 'scatters';
+		else if(playerMove===3) playerMove = 'stone';
+		document.getElementById('main-frame').innerHTML="Computer chose is "+computerMove+". Your move is "+playerMove+". "+result;
+	}

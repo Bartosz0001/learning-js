8553770e1dc510f7cfc6fb90d5f7042114cfa6c9
Bartosz Iwiński <01bartosz0001@gmail.com>
2019-08-06 23:14:29 +0200

    Change variable names, add new function in script.js that shorten code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
-	var ans1 = document.getElementById('ans1');
-	var ans2 = document.getElementById('ans2');
-	var ans3 = document.getElementById('ans3');
+	var paperbtn = document.getElementById('paper-btn');
+	var scissorsbtn = document.getElementById('scissors-btn');
+	var rockbtn = document.getElementById('rock-btn');
 	
 	var playerPoints = 0;
 	var computerPoints = 0;
 	
-	ans1.addEventListener("click", function() { check(1); });
-	ans2.addEventListener("click", function() { check(2); });
-	ans3.addEventListener("click", function() { check(3); });
+	paperbtn.addEventListener("click", function() { check(1); });
+	scissorsbtn.addEventListener("click", function() { check(2); });
+	rockbtn.addEventListener("click", function() { check(3); });
 	
 	function check(playerMove) {
 		let computerMove = Math.floor(Math.random() * 3 + 1);
@@ -22,12 +22,20 @@
 			result = 'You lose!';
 			computerPoints++;
 		}
-		if(computerMove===1) computerMove = 'paper';
-		else if(computerMove===2) computerMove = 'scatters';
-		else if(computerMove===3) computerMove = 'stone';
+		/*if(computerMove===1) computerMove = 'paper';
+		else if(computerMove===2) computerMove = 'scissors';
+		else if(computerMove===3) computerMove = 'rock';
 		if(playerMove===1) playerMove = 'paper';
-		else if(playerMove===2) playerMove = 'scatters';
-		else if(playerMove===3) playerMove = 'stone';
-		document.getElementById('main-frame').innerHTML="Computer chose is "+computerMove+". Your move is "+playerMove+". "+result;
+		else if(playerMove===2) playerMove = 'scissors';
+		else if(playerMove===3) playerMove = 'rock';*/
+		computerMove = change(computerMove);
+		playerMove = change(playerMove);
+		document.getElementById('main-frame').innerHTML="Computer choise is "+computerMove+". Your move is "+playerMove+". "+result;
 		document.getElementById('results').innerHTML="Player: "+playerPoints+" Computer: "+computerPoints;
+	}
+	
+	function change(number) {
+		if(number===1) return 'paper';
+		else if(number===2) return 'scissors';
+		else if(number===3) return 'rock';
 	}

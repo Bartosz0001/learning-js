c0676058c321d88c12e052e2b11fb475d2a02cc6
Bartosz Iwiński <01bartosz0001@gmail.com>
2019-08-11 13:20:19 +0200

    Change var to let in script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
-	var paperbtn = document.getElementById('paper-btn');
-	var scissorsbtn = document.getElementById('scissors-btn');
-	var rockbtn = document.getElementById('rock-btn');
+	const paperbtn = document.getElementById('paper-btn');
+	const scissorsbtn = document.getElementById('scissors-btn');
+	const rockbtn = document.getElementById('rock-btn');
 	
-	var playerPoints = 0;
-	var computerPoints = 0;
+	let playerPoints = 0;
+	let computerPoints = 0;
 	
 	paperbtn.addEventListener("click", function() { check(1); });
 	scissorsbtn.addEventListener("click", function() { check(2); });
 	rockbtn.addEventListener("click", function() { check(3); });
 	
-	function check(playerMove) {
+	function checkWinners(playerMove) {
 		let computerMove = Math.floor(Math.random() * 3 + 1);
 		let result;
 		if(computerMove===1 && playerMove===1 || computerMove===2 && playerMove===2 || computerMove===3 && playerMove===3) result = 'There is a draw.';
@@ -28,13 +28,13 @@
 		if(playerMove===1) playerMove = 'paper';
 		else if(playerMove===2) playerMove = 'scissors';
 		else if(playerMove===3) playerMove = 'rock';*/
-		computerMove = change(computerMove);
-		playerMove = change(playerMove);
+		computerMove = changeNumberToString(computerMove);
+		playerMove = changeNumberToString(playerMove);
 		document.getElementById('main-frame').innerHTML="Computer choise is "+computerMove+". Your move is "+playerMove+". "+result;
 		document.getElementById('results').innerHTML="Player: "+playerPoints+" Computer: "+computerPoints;
 	}
 	
-	function change(number) {
+	function changeNumberToString(number) {
 		if(number===1) return 'paper';
 		else if(number===2) return 'scissors';
 		else if(number===3) return 'rock';
